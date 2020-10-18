3c61d7c797c7318bc9fc8ad5c2ea1c82e6035c86
Rramadhan1818 <55430991+Rramadhan1818@users.noreply.github.com>
2020-10-18 16:27:06 +0700

    first commit

---
+++ src/if10k/pkg10119912/latihan13/relational/IF10K10119912Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119912.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * NAMA  : Rezki Ramadhan
+ * KELAS : IF10K
+ * NIM  `: 10119912
+ * Deskripsi Program : Program ini berisi proses operasi relational
+ * 
+ */
+public class IF10K10119912Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));        
+        System.out.println("a != b = " + (bil1 != bil2));        
+        System.out.println("a > b = " + (bil1 >  bil2));        
+        System.out.println("a < b = " + (bil1 <  bil2));        
+        System.out.println("b >= a = " + (bil2 >=  bil1));        
+        System.out.println("b <= a = " + (bil2 <=  bil1));
+    }
+    
+}
